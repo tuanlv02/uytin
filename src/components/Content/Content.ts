974b3d13287cd3df6d234abb546fa0beac389e9d
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2024-02-09 16:50:01 +0700

    chore: update UI

--- src/components/Content/Content.ts
+++ src/components/Content/Content.ts
@@ -13,9 +13,9 @@ const Content = {
       <b>UY TÍN</b> số 1 Việt Nam, mình nhận hỗ trợ quét mã Wechat cho tất cả anh/chị/bạn nào có nhu cầu ạ
     </p>
     <p class="mt-1">CAM KẾT:</p>
-    <ul class="list-disc">
-      <li>Nói <b>KHÔNG</b> với scam, lừa đảo</li>
-      <li><b>UY TÍN</b> số 1 Việt Nam</li>
+    <ul>
+      <li>&#x2022; Nói <b>KHÔNG</b> với scam, lừa đảo</li>
+      <li>&#x2022; <b>UY TÍN</b> số 1 Việt Nam</li>
     </ul>
     <p class="mt-3">Dưới đây là một số giao dịch của mình</p>
 
@@ -28,7 +28,7 @@ const Content = {
         ).format("HH:mm DD/MM/YYYY")}</p>
         <img src="/images/${item.image}" alt="Ảnh giao dịch số ${
           index + 1
-        }" class="w-96 mx-auto max-w-full border rounded-lg overflow-hidden" />
+        }" class="w-96 mx-auto max-w-full border rounded-lg overflow-hidden mt-1" />
       </div>`;
       })
       .join("")}`;
